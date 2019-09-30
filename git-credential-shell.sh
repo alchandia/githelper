085b22a0f600610ba303c5eacc6a7868c0894930
alchandia <alchandia@gmail.com>
2019-09-30 12:20:41 -0300

    Fix issue with similar names

--- git-credential-shell.sh
+++ git-credential-shell.sh
@@ -2,8 +2,8 @@
 
 REPO=$(basename $PWD)
 
-USER=$(cat $HOME/.githelperdata.txt | grep  $REPO | cut -d ";" -f 2)
-PASS=$(cat $HOME/.githelperdata.txt | grep  $REPO | cut -d ";" -f 3)
+USER=$(cat $HOME/.githelperdata.txt | grep  "$REPO;" | cut -d ";" -f 2)
+PASS=$(cat $HOME/.githelperdata.txt | grep  "$REPO;" | cut -d ";" -f 3)
 
 echo username=$USER
 echo password=$PASS
